4cebb9f4f6b581d7d5296ecca00820bec42636cb
Alina Salakhova <alina.salakhova1991@gmail.com>
2024-02-20 00:56:43 +0100

    Добавили реализацию функции фильтрации вакансий сохраненных в файле и их вывода.

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date
 import re
 from .vacancy import Vacancy
 
@@ -67,7 +67,8 @@ def continue_with_saved_file(vacancy_manager):
         print("1. Вывести полученные вакансии")
         print("2. Сравнить вакансии по зарплате")
         print("3. Удалить одну или несколько вакансий")
-        print("4. Выйти")
+        print("4. Отфильтровать вакансии и вывести")
+        print("5. Выйти")
 
         action_choice = input("Введите номер выбранного действия: ")
 
@@ -128,6 +129,27 @@ def continue_with_saved_file(vacancy_manager):
             print(f"Удаление вакансий выполнено. Осталось вакансий в файле: {len(saved_vacancies_after_deletion)}.")
 
         elif action_choice == '4':
+            # Предложить пользователю выбрать фильтры для вывода отфильтрованных вакансий
+            filters = get_filters()
+
+            if not filters:
+                print("Фильтры не были выбраны. Возвращаемся к основному меню.")
+            else:
+                # Загрузить вакансии из сохраненного файла
+                saved_vacancies = vacancy_manager.get_vacancies()
+
+                # Фильтрация вакансий на основе выбранных фильтров
+                filtered_vacancies = filter_vacancies_from_file(saved_vacancies, filters)
+
+                if not filtered_vacancies:
+                    print("По выбранным критериям вакансии не найдены.")
+
+                else:
+                    # Вывести отфильтрованные вакансии
+                    print_filtered_vacancies(
+                        filtered_vacancies)
+
+        elif action_choice == '5':
             print("Выход из программы выполнен. До свидания!")
             break  # Выход из цикла, если пользователь выбрал выход
 
@@ -135,7 +157,7 @@ def continue_with_saved_file(vacancy_manager):
             print("Некорректный выбор. Пожалуйста, выберите существующий номер действия.")
 
 
-# Функция для фильтрации вакансий
+# Функция для фильтрации вакансий во время поиска вакансий
 def filter_vacancies(vacancies, filters):
     filtered_vacancies = [Vacancy(vac_data) for vac_data in vacancies]
 
@@ -160,11 +182,66 @@ def filter_vacancies(vacancies, filters):
     return filtered_vacancies
 
 
+def filter_vacancies_from_file(vacancies, filters):
+    """Фильтрует вакансии на основе заданных критериев из списка вакансий, загруженных из файла."""
+    filtered_vacancies = []
+
+    for vac in vacancies:
+        match = True
+        for filter_key, filter_val in filters.items():
+            vac_val = None
+            # Преобразование значений для сравнения
+            if filter_key in ['зарплата от', 'зарплата до']:
+                filter_val = int(filter_val)
+                vac_val = int(vac.get(filter_key.replace(" ", "_"), 0))
+
+            elif filter_key == 'дата публикации':
+                vac_date = datetime.strptime(vac['published_at'], "%Y-%m-%dT%H:%M:%S%z").date()
+                start_date = datetime.strptime(filter_val, "%d.%m.%Y").date()
+                current_date = date.today()
+                if not (start_date <= vac_date <= current_date):
+                    match = False
+
+            elif filter_key == 'город' or filter_key == 'опыт работы':
+                vac_val = vac.get(filter_key.replace(" ", "_"), "").lower()
+                filter_val = filter_val.lower()
+
+            # Сравнение значений
+            if filter_key in ['зарплата от', 'зарплата до'] and not (vac_val >= filter_val):
+                match = False
+            elif filter_key in ['город', 'опыт работы'] and vac_val != filter_val:
+                match = False
+
+        if match:
+            filtered_vacancies.append(vac)
+
+    return filtered_vacancies
+
+
 # Функция для вывода топ N вакансий
 def print_top_vacancies(vacancies, top_n):
     sorted_vacancies = sorted(vacancies, key=lambda v: (v.salary_from or 0, v.salary_to or 0), reverse=True)[:top_n]
     print("\nТоп вакансий по вашему запросу:")
     for index, vac in enumerate(sorted_vacancies, start=1):
         print(f"Вакансия №{index}")
-        print_vacancy_details(vac)  # Эта функция уже должна быть определена в src/functions.py
+        print_vacancy_details(vac)
         print()
+
+
+def print_filtered_vacancies(vacancies):
+    print("По вашим фильтрам найдены следующие вакансии:")
+    for index, vacancy in enumerate(vacancies, start=1):
+        published_at_datetime = datetime.strptime(vacancy['published_at'], "%Y-%m-%dT%H:%M:%S%z")
+        published_at_formatted = published_at_datetime.strftime("%d.%m.%Y")
+
+        print(f"\nВакансия {index}:")
+        print(f"Название: {vacancy['name']}")
+        print(f"Ссылка: {vacancy['url']}")
+        print(f"Зарплата: от {vacancy.get('salary_from', 'не указано')} до {vacancy.get('salary_to', 'не указано')}")
+        print(f"Город: {vacancy['city']}")
+        print(f"Описание: {clean_highlight_tags(vacancy['description'])}")
+        print(f"Работодатель: {vacancy['employer']}")
+        print(f"Дата публикации: {published_at_formatted}")
+        print(f"Опыт работы: {vacancy['experience']}")
+        print(f"Тип занятости: {vacancy['employment_type']}")
+        print(f"График работы: {vacancy['schedule']}")
